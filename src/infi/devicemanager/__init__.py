f562b431dbec32ee8c7d8483c1de188b1129afa4
Guy Rozendorn <guy@rzn.co.il>
2012-04-01 12:36:29 +0300

    TRIVIAL raising KeyError if pdo is None

--- src/infi/devicemanager/__init__.py
+++ src/infi/devicemanager/__init__.py
@@ -64,7 +64,9 @@ class Device(object):
     @cached_method
     def psuedo_device_object(self):
         value = self._get_setupapi_property(properties.DEVPKEY_Device_PDOName)
-        return value if value is None else (GLOBALROOT + value)
+        if value is None:
+            raise KeyError(properties.DEVPKEY_Device_PDOName)
+        return GLOBALROOT + value
 
     @property
     @cached_method
